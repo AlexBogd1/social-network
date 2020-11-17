2a6a52bfbce2ba1470429a14badacac3aba2bc69
Alex <7894664@mail.ru>
2020-11-17 15:13:48 +0300

    create componentDidMount method

--- src/components/Users/UsersC.tsx
+++ src/components/Users/UsersC.tsx
@@ -26,15 +26,12 @@ export type UsersFromApiType = {
 
 class Users extends React.Component<UsersForPageType> {
 
-    constructor(props: UsersForPageType) {
-        super(props);
-
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
             // debugger
             this.props.setUsers(response.data.items);
         })
     }
-
 
     render() {
         return (<div>
