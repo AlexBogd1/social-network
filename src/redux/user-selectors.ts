d2981fc2e43d59eb0f4f6f3bbfb835ad816782dc
Alex <7894664@mail.ru>
2021-01-04 00:19:39 +0300

    create user selectors with reselect

--- src/redux/user-selectors.ts
+++ src/redux/user-selectors.ts
@@ -1,9 +1,23 @@
 import {ReduxStoreType} from "./redux-store";
 import {UsersFromApiType} from "../components/Users/Users";
+import {createSelector} from "reselect";
 
 export const getUsers = (store: ReduxStoreType): Array<UsersFromApiType> => {
     return store.usersPage.users
 }
+
+// export const getUsersSelector = (store: ReduxStoreType): Array<UsersFromApiType> => {
+//     return getUsers(store).filter(u => true);
+// }
+
+export const getIsFetching = (store: ReduxStoreType): boolean => {
+    return store.usersPage.isFetching
+}
+
+export const getUsersSuperSelector = createSelector(getUsers,getIsFetching,(users,isFetching) => {
+     return  users.filter(u => true);
+});
+
 export const getPageSize = (store: ReduxStoreType): number => {
     return store.usersPage.pageSize
 }
@@ -14,9 +28,7 @@ export const getTotalUsersCount = (store: ReduxStoreType): number => {
 export const getCurrentPage = (store: ReduxStoreType): number => {
     return store.usersPage.currentPage
 }
-export const getIsFetching = (store: ReduxStoreType): boolean => {
-    return store.usersPage.isFetching
-}
+
 export const getFollowingInProgress = (store: ReduxStoreType): string[] => {
     return store.usersPage.followingInProgress
 }
