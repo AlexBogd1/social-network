09b3d927bb401794a9c2670d1b8b436134b137cd
Alex <7894664@mail.ru>
2021-01-20 15:23:08 +0300

    add lazy component inside Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
-import {Route, withRouter} from "react-router-dom";
+import { Route, withRouter } from "react-router-dom";
 import UsersContainer from "./components/Users/UsersContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
-import {compose} from "redux";
-import {connect} from "react-redux";
-import {ReduxStoreType} from "./redux/redux-store";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { ReduxStoreType } from "./redux/redux-store";
 import Preloader from "./components/common/Preloader";
-import {initializeApp} from "./redux/app-reducer";
+import { initializeApp } from "./redux/app-reducer";
 
 type AppComponentType = {
     initialized: boolean
@@ -26,29 +26,33 @@ class App extends React.Component<AppComponentType> {
     }
 
     render() {
-        if(!this.props.initialized) {
-            return <Preloader/>
+        if (!this.props.initialized) {
+            return <Preloader />
         }
 
 
         return (
-                <div className="app-wrapper">
-                    <HeaderContainer/>
-                    <NavBar/>
-                    <div className="app-wrapper-content">
-                        <Route path="/dialogs" render={() => <DialogsContainer/>}/>
-                        <Route path="/profile/:userID?" render={() => <ProfileContainer/>}/>
-                        <Route path="/users" render={() => <UsersContainer/>}/>
-                        <Route path="/login" render={() => <Login/>}/>
-                    </div>
+            <div className="app-wrapper">
+                <HeaderContainer />
+                <NavBar />
+                <div className="app-wrapper-content">
+                    <Route path="/dialogs" render={() => <Suspense fallback={<Preloader />}>
+                        <DialogsContainer />
+                    </Suspense>} />
+                    <Route path="/profile/:userID?" render={() => <Suspense fallback={<Preloader />}>
+                        <ProfileContainer />
+                    </Suspense>} />
+                    <Route path="/users" render={() => <UsersContainer />} />
+                    <Route path="/login" render={() => <Login />} />
                 </div>
+            </div>
 
         );
     }
 }
 
-const mapStateToProps = (state:ReduxStoreType) => ({
+const mapStateToProps = (state: ReduxStoreType) => ({
     initialized: state.app.initialized
 })
 
-export default compose<React.ComponentType>(connect(mapStateToProps, {initializeApp}), withRouter)(App)
+export default compose<React.ComponentType>(connect(mapStateToProps, { initializeApp }), withRouter)(App)
