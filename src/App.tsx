ab5a6e7b5765a64d5ff01ca20cd4d7c0b51b1281
AlexB <7894664@mail.ru>
2021-01-31 20:32:50 +0300

    create 404 Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
-import { Route, withRouter } from "react-router-dom";
+import { Route, Switch, withRouter } from "react-router-dom";
 import UsersContainer from "./components/Users/UsersContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
@@ -12,47 +12,64 @@ import Preloader from "./components/common/Preloader";
 import { initializeApp } from "./redux/app-reducer";
 
 type AppComponentType = {
-    initialized: boolean
-    initializeApp: () => void
-}
+  initialized: boolean;
+  initializeApp: () => void;
+};
 
-const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'))
-const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'))
+const DialogsContainer = React.lazy(
+  () => import("./components/Dialogs/DialogsContainer")
+);
+const ProfileContainer = React.lazy(
+  () => import("./components/Profile/ProfileContainer")
+);
 
 class App extends React.Component<AppComponentType> {
+  componentDidMount() {
+    this.props.initializeApp();
+  }
 
-    componentDidMount() {
-        this.props.initializeApp();
+  render() {
+    if (!this.props.initialized) {
+      return <Preloader />;
     }
 
-    render() {
-        if (!this.props.initialized) {
-            return <Preloader />
-        }
-
-
-        return (
-            <div className="app-wrapper">
-                <HeaderContainer />
-                <NavBar />
-                <div className="app-wrapper-content">
-                    <Route path="/dialogs" render={() => <Suspense fallback={<Preloader />}>
-                        <DialogsContainer />
-                    </Suspense>} />
-                    <Route path="/profile/:userID?" render={() => <Suspense fallback={<Preloader />}>
-                    <ProfileContainer />
-                    </Suspense>} />
-                    <Route path="/users" render={() => <UsersContainer />} />
-                    <Route path="/login" render={() => <Login />} />
-                </div>
-            </div>
-
-        );
-    }
+    return (
+      <div className="app-wrapper">
+        <HeaderContainer />
+        <NavBar />
+        <div className="app-wrapper-content">
+          <Switch>
+            <Route
+              path="/dialogs"
+              render={() => (
+                <Suspense fallback={<Preloader />}>
+                  <DialogsContainer />
+                </Suspense>
+              )}
+            />
+            <Route
+              path="/profile/:userID?"
+              render={() => (
+                <Suspense fallback={<Preloader />}>
+                  <ProfileContainer />
+                </Suspense>
+              )}
+            />
+            <Route path="/users" render={() => <UsersContainer />} />
+            <Route path="/login" render={() => <Login />} />
+            <Route path ='*' render={() => <div>404 Not Found</div>}/>
+          </Switch>
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state: ReduxStoreType) => ({
-    initialized: state.app.initialized
-})
+  initialized: state.app.initialized,
+});
 
-export default compose<React.ComponentType>(connect(mapStateToProps, { initializeApp }), withRouter)(App)
+export default compose<React.ComponentType>(
+  connect(mapStateToProps, { initializeApp }),
+  withRouter
+)(App);
